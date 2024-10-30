5c68a401c166b5a3003e12da6113adb0ea8c7df3
RobertLib <rob@hotmail.cz>
2024-10-30 21:35:31 +0100

    Changes 10/15/2024

--- map/map.gd
+++ map/map.gd
@@ -2,10 +2,22 @@ extends Node2D
 
 var timer := 0.0
 
+@onready var map1 := $Map1 as Sprite2D
+@onready var map2 := $Map2 as Sprite2D
+@onready var maps := [map1, map2]
+@onready var label1 := $CanvasLayer/MarginContainer/MarginContainer/Label1 as Label
+@onready var label2 := $CanvasLayer/MarginContainer/MarginContainer/Label2 as Label
+@onready var labels := [label1, label2]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
+	if Globals.level in range(1, 3):
+		maps[0].visible = true
+		labels[0].visible = true
+	elif Globals.level in range(3, 6):
+		maps[1].visible = true
+		labels[1].visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
