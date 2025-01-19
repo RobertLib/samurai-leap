8a34e4cfc46a17a45e1b18a26a76ecc3f6e02f2e
RobertLib <rob@hotmail.cz>
2025-01-19 17:31:26 +0100

    Changes 01/04/2024

--- map/map.gd
+++ map/map.gd
@@ -5,9 +5,11 @@ var timer := 0.0
 @onready var map1 := $Map1 as Sprite2D
 @onready var map2 := $Map2 as Sprite2D
 @onready var map3 := $Map3 as Sprite2D
+@onready var map4 := $Map4 as Sprite2D
 @onready var label1 := $CanvasLayer/MarginContainer/MarginContainer/Label1 as Label
 @onready var label2 := $CanvasLayer/MarginContainer/MarginContainer/Label2 as Label
 @onready var label3 := $CanvasLayer/MarginContainer/MarginContainer/Label3 as Label
+@onready var label4 := $CanvasLayer/MarginContainer/MarginContainer/Label4 as Label
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +23,9 @@ func _ready() -> void:
 	elif Globals.level in range(6, 9):
 		map3.visible = true
 		label3.visible = true
+	elif Globals.level in range(9, 12):
+		map4.visible = true
+		label4.visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
